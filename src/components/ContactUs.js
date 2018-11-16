07db29cb8a176f92b355347d4dfcb36a8fc6d862
scliff6262 <sclifford6262@gmail.com>
2018-11-16 12:27:38 -0500

    created simple Contact page

---
+++ src/components/ContactUs.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Well } from 'react-bootstrap'
+
+const ContactUs = () => {
+  return(
+    <Well bsSize="large" style={{maxWidth: "1000px", margin: "auto", textAlign: "center"}}>
+      <h3>Contact Us</h3>
+      <br />
+      <h5>Retail Fund Marketing <br/> <small>Open-End and Closed-End Funds</small></h5>
+      <p><i>800-GABELLI</i></p>
+      <h5>Institutional Mutual Fund Marketing</h5>
+      <p><i>800-422-2274</i></p>
+      <h5>Seperate Accounts Marketing</h5>
+      <p><i>800-331-7266</i></p>
+      <h5>Open-End Fund Shareholder Services</h5>
+      <p><i>800-872-5365</i></p>
+      <h5>Closed-End Fund Shareholder Services</h5>
+      <p><i>800-336-6983</i></p>
+      <br/>
+    </Well>
+  )
+}
+
+export default ContactUs
+
+/*
+Telephone Numbers
+Retail Fund Marketing
+(Open-End and Closed-End Funds)800-GABELLI (800-422-3554)
+Institutional Mutual Fund Marketing800-422-2274
+Separate Accounts Marketing800-331-7266
+Open-End Fund Shareholder Services800-872-5365
+Closed-End Fund Shareholder Services800-336-6983
+*/
